ee0029ef600ab1c55271d498856eacb990f8c7bf
Kajal Sharma <divyansh_213042@saitm.org>
2025-08-02 22:57:34 +0530

    React Practice

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+// import React ,{useState} from "react";
+
+// const App = () => {
+// // const a = 10
+
+// const [a,setA] = useState(20)
+
+// const changeA =() =>{
+//         console.log("chal gya")
+//         setA(50)
+// }
+
+//   return (
+//     <div>
+//       <h1>Value of a is {a}</h1>
+
+//       <button onClick={changeA} class="button"> Change User</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+// import React,{useState} from 'react'
+
+// const App = () => {
+// const [num, setNum] = useState(0);
+
+//         return(
+//                 <div>
+//                         <h3>Number is {num}</h3>
+//                         <button onClick={()=> setNum (num+10)} class="button">Increase Value</button>
+//                         <button onClick= {()=>setNum(num - 10)} class="button">Decrease Value</button>
+//                 </div>
+//         )
+// }
+
+// export default App
+
+
+// from handler start here
+// two way binding in react 
+import React,{useState} from 'react'
+
+const App = () => {
+
+       
+        const [username, setUsername] = useState('')
+
+        const submitHandler = (e) => {
+                e.preventDefault()
+                console.log(username);
+                setUsername('')
+                
+        }
+        return(
+                <div>
+                      <form onSubmit = {(e)=>{
+                        submitHandler(e)
+                      }}>
+                        <input 
+                        value={username}
+                        onChange={(e)=>{
+                                setUsername(e.target.value)
+                        }}  
+                        className='px-4 py-3 rounded text-xl m-5' type="text" placeholder="Enter your name"></input>
+                        <button className='px-4 py-3 text-white m-5 font-semibold bg-emerald-600 rounded'>
+                                Submit
+                        </button>
+                      </form>
+                </div>
+        )
+
+}
+
+export default App
+
+// from handler end here
