6a170c9707db59eadd9fb71f08c273c2357d9260
Kajal Sharma <divyansh_213042@saitm.org>
2025-08-03 23:30:59 +0530

    API fetch axios data

--- src/App.jsx
+++ src/App.jsx
@@ -204,21 +204,59 @@
 
 
 
-import React from 'react'
-import Ex from './components/Ex'
+// import React from 'react'
+// import Ex from './components/Ex'
 
 
-const App =() => {
-        return (
-                <div>
-                        <Ex name="Divyansh Gupta"/>
-                </div>
-        )
-}
+// const App =() => {
+//         return (
+//                 <div>
+//                         <Ex name="Divyansh Gupta"/>
+//                 </div>
+//         )
+// }
 
-export default App
+// export default App
 
 // ============================================
 
 
 // ================================================= 
+
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
+
+const App =  () =>{
+
+const [data, setData] = useState([])
+
+const getData = async ()  => {
+        const response = await axios.get('https://picsum.photos/v2/list')
+        // console.log(response.data)
+        setData(response.data)
+        
+}
+
+useEffect (() => {
+        getData()
+},[])
+
+        return (
+               
+                <div className='p-10'>
+                <button onClick={getData} className='bg-pink-700 text-white font-semibold text-2xl px-6 py-3 rounded active:scale-90'>Get Data</button>
+                <div className='p-5 mt-5 bg-gray-950'>
+                        {data.map(function(elem, idx){
+                                return <div key={idx} className='bg-gray-50 text-black flex item-center w-full justify-between px-7 py-6 rounded mb-2'>
+                                      <img className='h-40' src={elem.download_url} alt=""/>  
+                                      <h1>{elem.author}</h1>
+
+                                </div>
+                        })}
+                </div>
+                </div>
+                
+
+        )
+}
+export default App
