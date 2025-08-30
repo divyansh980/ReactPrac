a2372e0ab5e55de0058ac556c305149ccd89e4e5
Kajal Sharma <divyansh_213042@saitm.org>
2025-08-30 23:04:38 +0530

    Documentation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,9 @@
+/**
+ * The Contact component is a functional React component that renders a simple Contact Page with a
+ * heading.
+ * @returns The Contact component is being returned, which is a functional component that renders a div
+ * containing an h1 element with the text "Contact Page".
+ */
 import React from 'react'
 
 const Contact = () => {
