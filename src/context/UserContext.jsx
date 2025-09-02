1683d2320fc8ede8baa56c477fa564758c6e2766
Kajal Sharma <divyansh_213042@saitm.org>
2025-09-02 21:43:55 +0530

    just push

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,3 +1,10 @@
+/**
+ * The UserContext component creates a context with user data and provides it to its children
+ * components.
+ * @returns The `UserContext` component is being returned, which wraps its children with a
+ * `DataContext.Provider` component. The `userData` object containing information like username, age,
+ * city, and college is provided as the value to the `DataContext.Provider`.
+ */
 
 import { createContext } from 'react'
 
