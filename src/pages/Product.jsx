02766807a60f8e8031b5d98f7247a6f38fbcc932
Kajal Sharma <divyansh_213042@saitm.org>
2025-09-02 21:42:49 +0530

    just push

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,9 @@
+/**
+ * The above function is a React component named Product that renders a simple product page with a
+ * heading.
+ * @returns The `Product` component is being returned, which is a simple functional component in React
+ * that renders a `div` containing an `h1` element with the text "Product Page".
+ */
 import React from 'react'
 
 const Product = () => {
